15cb2d94d454691ad9d151dd449bc88e0a072ef1
pphU00123 <s42884@bj.ac.th>
2024-11-22 15:41:26 +0700

    Add files via upload

---
+++ a2_direction_32/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1732264594,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/collide-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/playeranimation-sheet0.png",
+		"images/playeranimation-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
