13aeb482c2557c657d6081906c77567e385346f8
pphU00123 <s42884@bj.ac.th>
2025-01-27 15:26:18 +0700

    Add files via upload

---
+++ a5_flappingbird_26/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1737966297,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/background-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/pipe_buttom-sheet0.png",
+		"images/floor1.png",
+		"images/floor2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
